738b8f830159e82fc15fed6c557506b4e7d51a96
mvitorferraz <141591608+mvitorferraz@users.noreply.github.com>
2023-11-09 01:36:53 +0000

    0.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
+      <div className="card">
+        <h2>Morty Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" />
+      </div>
     </>
   )
 }
